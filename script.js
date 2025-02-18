1489efa38536a9c0837acebba800db9378a982af
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-02-18 16:43:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gameArena = document.getElementById("game-arena");
+    const arenaSize = 600;
+    const cellSize = 20;
+    let score = 0;
+    let gameStarted = false;
+    let food = {x: 300, y: 200};
+    let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}];
+
+});
